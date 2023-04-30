c3aaf9b8ae22580a6e38c39d376d57e2ffed456d
Samuel Lee <samuellee@lev-11289.apn.wlan.private.upenn.edu>
2023-04-30 13:16:10 -0400

    added all pm4 routes

--- server/routes.js
+++ server/routes.js
@@ -131,6 +131,37 @@ const search_songs_advanced = async function(req, res) {
   });
 }
 
+const top_artist_by_country = async function(req, res) {
+  connection.query(`
+  CREATE INDEX listeners_index
+  ON Artists(listeners);
+  
+  WITH top_in_country AS (
+    SELECT a1.country, MAX(a1.listeners) AS listeners
+    FROM Artists a1
+    JOIN (
+    SELECT a2.artist, a2.tags
+    FROM Artists a2
+    WHERE (a1.country = a2.country) AND
+      (a2.listeners = MAX(a1.listeners))
+  ) info ON a1.country = info.country
+    GROUP BY country
+  )
+  SELECT tic.country, tic.artist, tic.tags, tic.listeners,
+    rat.Title AS album, ‘Metacritic Critic Score’ AS
+    critic_score
+  FROM top_in_country tic JOIN Ratings rat
+  ON UPPER(tic.artist) = UPPER(rat.Artist)  
+  `, (err, data) => {
+    if (err || data.length === 0) {
+      console.log(err);
+      res.json([]);
+    } else {
+      res.json(data);
+    }
+  }); 
+}
+
 const rating_threshold_count = async function(req, res) {
   const threshold = req.query.threshold ?? 0;
 
@@ -141,6 +172,60 @@ const rating_threshold_count = async function(req, res) {
     GROUP BY Artist
     ORDER BY COUNT(*) DESC
     `, (err, data) => {
+    if (err || data.length === 0) {
+      console.log(err);
+      res.json([]);
+    } else {
+      res.json(data);
+    }
+  }); 
+}
+
+const average_rating_artist = async function(req, res) {
+  const artist = req.params.artist ?? ''
+  connection.query(`
+  SELECT Artist, AVG(Rating) AvgRating
+  FROM Reviews
+  WHERE Artist = ${artist}
+  GROUP BY Artist
+  ORDER BY AvgRating DESC
+  `, (err, data) => {
+    if (err || data.length === 0) {
+      console.log(err);
+      res.json([]);
+    } else {
+      res.json(data);
+    }
+  }); 
+}
+
+const average_albums = async function(req, res) {
+  connection.query(`
+  WITH meta AS (
+    SELECT Artist, DISTINCT Title, ‘Metacritic Critic Score’ AS 
+        critic_score, ‘Metacritic User Score’ AS
+        user_score
+    FROM Ratings
+  ),
+  critic_reviews AS (
+    SELECT Artist, DISTINCT Title, AVG(Rating) as review_scores
+    FROM Reviews
+    GROUP BY (Artist, Title)
+  ),
+  albums AS (
+    SELECT DISTINCT album, artists, AVG(danceability) AS
+    danceability, SUM(duration_ms) / 6000 AS
+    duration_min, release_date
+  FROM Songs
+  GROUP BY album
+  ),
+  SELECT m.Title, m.Artist, m.critic_score, m.user_score, 
+      cr.review_scores, a.danceability, a.duration_min,
+      a.release_date
+  FROM meta m JOIN critic_reviews cr ON m.Title = cr.Title
+  JOIN albums a ON m.Title = a.album 
+  ORDER BY m.critic_score DESC  
+  `, (err, data) => {
     if (err || data.length === 0) {
       console.log(err);
       res.json([]);
@@ -195,13 +280,77 @@ const top_albums_in_range = async function(req, res) {
   });
 }
 
-// ADD ROUTE VARS BELOW
+const highest_rated_albums_per_artist = async function(req, res) {
+  connection.query(`
+    WITH avg_ratings AS (
+      SELECT Artist, Title, AVG(Rating)AS avg_rating
+      FROM Reviews
+      GROUP BY Artist, Title
+    ),
+    album_mc_scores AS (
+      SELECT Album, Metacritic_User_Score
+      FROM Albums
+    ), 
+    max_album AS (
+      SELECT a.Artist, a.Title, a.avg_rating
+      FROM avg_ratings a
+      WHERE avg_rating = MAX(SELECT MAX(b.avg_rating)
+                FROM avg_ratings b
+                WHERE a.Artist = b.Artist
+    ),
+    danceabilities AS (
+      SELECT album, AVG(danceability) AS avg_dance
+      FROM Song
+      GROUP BY album
+    ),
+    dance_and_rating AS (
+      SELECT a.Title AS album, b.avg_dance as avg_dance, a.avg_rating AS avg_rating
+      FROM max_album a JOIN danceabilities b
+                      ON a.Title = b.album
+    ),
+    SELECT a.album AS album, a.avg_dance AS avg_dance, a.avg_rating AS avg_rating, b.Metacritic_User_Score AS Metacritic_User_Score
+    FROM dance_and_rating a JOIN album_mc_scores b
+                ON a.album = b.Album
+    ORDER BY avg_dance, avg_rating, Metacritic_User_Score
+  `, (err, data) => {
+    if (err || data.length === 0) {
+      console.log(err);
+      res.json([]);
+    } else {
+      res.json(data);
+    }
+  }); 
+}
+
+const average_country_rating = async function(req, res) {
+  const country = req.query.country ?? '';
+  connection.query(`
+    SELECT AVG(Rating), country
+    FROM Reviews JOIN Artist ON Artist.artist = Reviews.Artist
+    WHERE country = ${country}
+    GROUP BY country
+    ORDER BY AVG(Rating) DESC
+  `, (err, data) => {
+    if (err || data.length === 0) {
+      console.log(err);
+      res.json([]);
+    } else {
+      res.json(data);
+    }
+  }); 
+}
+
 module.exports = {
   search_artists,
   search_albums,
   search_songs,
   num_artists_by_country,
   search_songs_advanced,
+  top_artist_by_country,
   rating_threshold_count,
+  average_rating_artist,
+  average_albums,
   top_albums_in_range,
+  highest_rated_albums_per_artist,
+  average_country_rating
 }
--- server/server.js
+++ server/server.js
@@ -15,7 +15,13 @@ app.get('/searchAlbums', routes.search_albums);
 app.get('/searchSongs', routes.search_songs);
 app.get('/numArtistsByCountry', routes.num_artists_by_country)
 app.get('/searchSongsAdvanced', routes.search_songs_advanced)
-app.get('/top_albums_in_range', routes.top_albums_in_range)
+app.get('/topArtistByCountry', routes.top_artist_by_country)
+app.get('/ratingThresholdCount', routes.rating_threshold_count)
+app.get('/averageRating/:artist', routes.average_rating_artist)
+app.get('/averageAlbums', routes.average_albums)
+app.get('/topAlbumsInRange', routes.top_albums_in_range)
+app.get('/highestRatedAlbumsPerArtist', routes.highest_rated_albums_per_artist)
+app.get('/averageCountryRating/:country', routes.average_country_rating)
 
 
 app.listen(config.server_port, () => {
@@ -23,3 +29,4 @@ app.listen(config.server_port, () => {
 });
 
 module.exports = app;
+
